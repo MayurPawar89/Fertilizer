8368be2b37573c3989871334e88c3cfa8469f94e
UmeshNit <umeshnitnaware@gmail.com>
2015-06-27 11:24:14 +0530

    Umesh: Added Design forms for Receipt

---
+++ FertilizerProject/FertilizerProject/Edit/FrmVoucherReciept.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace FertilizerProject.Edit
+{
+    partial class FrmVoucherReciept
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnH = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnBankRecon = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnReport = new System.Windows.Forms.Button();
+            this.txtPaymentdetails = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cmbPaymentmode = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDebitacct = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtPayment = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtManualno = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPaymentno = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnH
+            // 
+            this.btnH.Location = new System.Drawing.Point(741, 419);
+            this.btnH.Name = "btnH";
+            this.btnH.Size = new System.Drawing.Size(24, 28);
+            this.btnH.TabIndex = 49;
+            this.btnH.Text = "H";
+            this.btnH.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(654, 419);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(81, 28);
+            this.btnClose.TabIndex = 48;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(567, 419);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(81, 28);
+            this.btnCancel.TabIndex = 47;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnBankRecon
+            // 
+            this.btnBankRecon.Location = new System.Drawing.Point(402, 419);
+            this.btnBankRecon.Name = "btnBankRecon";
+            this.btnBankRecon.Size = new System.Drawing.Size(159, 28);
+            this.btnBankRecon.TabIndex = 46;
+            this.btnBankRecon.Text = "Bank Reconciliation";
+            this.btnBankRecon.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(333, 419);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(63, 28);
+            this.btnDelete.TabIndex = 45;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnModify
+            // 
+            this.btnModify.Location = new System.Drawing.Point(264, 419);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(63, 28);
+            this.btnModify.TabIndex = 44;
+            this.btnModify.Text = "Modify";
+            this.btnModify.UseVisualStyleBackColor = true;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(195, 419);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(63, 28);
+            this.btnPrint.TabIndex = 43;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(126, 419);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(63, 28);
+            this.btnSave.TabIndex = 42;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnReport
+            // 
+            this.btnReport.Location = new System.Drawing.Point(-81, 411);
+            this.btnReport.Name = "btnReport";
+            this.btnReport.Size = new System.Drawing.Size(63, 28);
+            this.btnReport.TabIndex = 41;
+            this.btnReport.Text = "Report";
+            this.btnReport.UseVisualStyleBackColor = true;
+            // 
+            // txtPaymentdetails
+            // 
+            this.txtPaymentdetails.Location = new System.Drawing.Point(192, 321);
+            this.txtPaymentdetails.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPaymentdetails.Name = "txtPaymentdetails";
+            this.txtPaymentdetails.Size = new System.Drawing.Size(485, 20);
+            this.txtPaymentdetails.TabIndex = 40;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(64, 327);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(113, 18);
+            this.label8.TabIndex = 39;
+            this.label8.Text = "Payment Details:";
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(192, 278);
+            this.cmbCategory.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(155, 21);
+            this.cmbCategory.TabIndex = 38;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(109, 281);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 18);
+            this.label7.TabIndex = 37;
+            this.label7.Text = "Category:";
+            // 
+            // cmbPaymentmode
+            // 
+            this.cmbPaymentmode.FormattingEnabled = true;
+            this.cmbPaymentmode.Location = new System.Drawing.Point(192, 231);
+            this.cmbPaymentmode.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbPaymentmode.Name = "cmbPaymentmode";
+            this.cmbPaymentmode.Size = new System.Drawing.Size(151, 21);
+            this.cmbPaymentmode.TabIndex = 36;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(74, 234);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 18);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "Payment type:";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(192, 188);
+            this.txtAmount.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(151, 20);
+            this.txtAmount.TabIndex = 34;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(118, 191);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 18);
+            this.label5.TabIndex = 33;
+            this.label5.Text = "Amount:";
+            // 
+            // txtDebitacct
+            // 
+            this.txtDebitacct.Location = new System.Drawing.Point(192, 137);
+            this.txtDebitacct.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDebitacct.Name = "txtDebitacct";
+            this.txtDebitacct.Size = new System.Drawing.Size(485, 20);
+            this.txtDebitacct.TabIndex = 32;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(81, 140);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 18);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "Credit Account:";
+            // 
+            // dtPayment
+            // 
+            this.dtPayment.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtPayment.Location = new System.Drawing.Point(192, 88);
+            this.dtPayment.Margin = new System.Windows.Forms.Padding(4);
+            this.dtPayment.Name = "dtPayment";
+            this.dtPayment.Size = new System.Drawing.Size(151, 20);
+            this.dtPayment.TabIndex = 30;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(85, 90);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 18);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Reciept Date:";
+            // 
+            // txtManualno
+            // 
+            this.txtManualno.Location = new System.Drawing.Point(515, 32);
+            this.txtManualno.Margin = new System.Windows.Forms.Padding(4);
+            this.txtManualno.Name = "txtManualno";
+            this.txtManualno.Size = new System.Drawing.Size(125, 20);
+            this.txtManualno.TabIndex = 28;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(425, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 18);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Manual No:";
+            // 
+            // txtPaymentno
+            // 
+            this.txtPaymentno.Location = new System.Drawing.Point(192, 27);
+            this.txtPaymentno.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPaymentno.Name = "txtPaymentno";
+            this.txtPaymentno.Size = new System.Drawing.Size(125, 20);
+            this.txtPaymentno.TabIndex = 26;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(88, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Reciept No:";
+            // 
+            // FrmVoucherReciept
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 480);
+            this.Controls.Add(this.btnH);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnBankRecon);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnReport);
+            this.Controls.Add(this.txtPaymentdetails);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cmbPaymentmode);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDebitacct);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dtPayment);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtManualno);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPaymentno);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmVoucherReciept";
+            this.Text = "FrmVoucherReciept";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnH;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnBankRecon;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnReport;
+        private System.Windows.Forms.TextBox txtPaymentdetails;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cmbPaymentmode;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDebitacct;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtPayment;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtManualno;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPaymentno;
+        private System.Windows.Forms.Label label1;
+    }
+}
